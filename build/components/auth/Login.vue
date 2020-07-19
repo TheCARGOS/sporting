c918b54080c3e8db6e0127e48b1c7deb36cdfe59
TheCARGOS <loayzavelez@gmail.com>
2020-07-19 01:10:21 -0500

    form redesigned

--- build/components/auth/Login.vue
+++ build/components/auth/Login.vue
@@ -1,27 +1,29 @@
 <template>
     <div class="container flex-column">
-        <div class="flex-container" v-if="!loggedIn">
+        <div class="main-login" v-if="!loggedIn">
             <div class="player-item">
                 <div class="flex-column">
-                    <div class="player-item__body">
-                        <span class="player-item__name">JUGADOR</span>
-                    </div>
                     <div class="player-item__header">
                         <img class="player-item__img"  src="https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/user.png" alt="">
                         <span class="player-item__span" >??</span>
                     </div>
+                    <div class="player-item__body">
+                        <span class="player-item__name">JUGADOR</span>
+                    </div>
                 </div>
             </div>
 
-            <form @submit.prevent="login">
+            <form @submit.prevent="login" class="container">
                 <fieldset>
-                    <input id="username" v-model="username" type="text" placeholder="username">
+                    <label for="username">USUARIO</label>
+                    <input id="username" v-model="username" type="text">
                 </fieldset>
 
                 <fieldset>
-                    <input id="password" v-model="password" type="password" placeholder="password">
+                    <label for="password">CONTRASEÑA</label>
+                    <input id="password" v-model="password" type="password">
                 </fieldset>
-                <button type="submit" class="player-stats__form__button">INGRESAR</button>
+                <button type="submit">INGRESAR</button>
             </form>
         </div>
 
@@ -72,23 +74,54 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    form {
-        width: 90%;
+    .main-login {
+        background-color: white;
+        color: #727272;
+        padding: 0 2em;
+        min-height: 70vh;
     }
 
-    input {
-        border: none;
-        font-size: 1.5em;
-        padding: 0 1em;
+    .player-item {
+        margin-top: 1.58em;
+    }
+
+    form {
         width: 100%;
-        border-radius: 10px;
-        line-height: 1.3;
         background-color: white;
     }
 
-    fieldset {
-        margin: 1.75em 0;
+    label {
+        font-size: 12px;
+        font-family: "Segoe UI";
+        font-weight: bolder;
+        margin-bottom: 8px;
+        line-height: 2;
     }
+
+    fieldset {
+        margin-bottom: 1em;
+    }
+
+    input {
+        font-size: 12px;
+        padding: 0 0.66em;
+        width: 100%;
+        line-height: 2;
+        border: 1px solid #C5C5C5;
+    }
+
+    button {
+        background: #46ABF8;
+        color: white;
+        font-family: "Segoe UI";
+        border: none;
+        display: block;
+        width: 100%;
+        margin-top: 30px;
+        font-weight: bolder;
+        line-height: 2;
+    }
+
 
     .profile-section {
         margin-top: 30px;
